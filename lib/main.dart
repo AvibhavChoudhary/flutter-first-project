cbd85ed5064d33a5c75b085aeae33ec05c540bc2
AvibhavChoudhary <avibhavchoudhary@gmail.com>
2020-10-04 16:17:48 +0530

    Data fetching from API done.

--- lib/main.dart
+++ lib/main.dart
@@ -12,32 +12,3 @@ void main() {
 
 
 
-// class DemoBox extends StatelessWidget {
-//   const DemoBox({Key key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       height: 200,
-//       width: 200,
-//       alignment: Alignment.center,
-//       padding: const EdgeInsets.all(8),
-//       decoration: BoxDecoration(
-//           boxShadow: [
-//             BoxShadow(
-//                 color: Colors.grey[500], blurRadius: 10, offset: Offset(6, 10))
-//           ],
-//           borderRadius: BorderRadius.circular(25),
-//           gradient:
-//               LinearGradient(colors: [Colors.redAccent, Colors.orangeAccent])),
-//       child: Text(
-//         "This is Fultter demo",
-//         style: TextStyle(
-//             color: Colors.white,
-//             fontWeight: FontWeight.w800,
-//             fontSize: 15,
-//             fontStyle: FontStyle.italic),
-//       ),
-//     );
-//   }
-// }
--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../card.dart';
 import '../drawer.dart';
-
+import 'package:http/http.dart' as http;
+import "dart:convert";
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -14,10 +15,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _controller = TextEditingController();
   var myText = "Change Me";
+  var url = "http://jsonplaceholder.typicode.com/photos";
+  var data;
 
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    setState(() {});
   }
 
   @override
@@ -32,16 +42,26 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(16.0),
         child: Align(
           alignment: Alignment.center,
-          child: SingleChildScrollView(
-            child: Card(
-              child: Change_name(myText: myText, controller: _controller),
-            ),
-          ),
+          child: data != null
+              ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      subtitle: Text("Id:${data[index]["id"]}"),
+                      leading: Image.network(data[index]["url"]),
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              )
+              : CircularProgressIndicator(),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          myText= _controller.text;
+          myText = _controller.text;
           setState(() {});
         },
         child: Icon(Icons.refresh),
